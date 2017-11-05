d3db310facc0ff4ce7d60c31f22ec4a6d378e693
TYKUHN2 <tyler.h.kuhn@gmail.com>
2017-11-05 04:41:05 -0500

    fixs seans bull

--- webpage/script/Draw.js
+++ webpage/script/Draw.js
@@ -30,3 +30,46 @@ function drawLoot(){
 function closeLoot(){
   $("#Loot").hide();
 }
+
+function updateRoom(){
+  updatePlayer();
+  $("#roomNum").val("Room#: " + rooms);
+  updateEnemies();
+}
+function updatePlayer () {
+  $("#atk").val("Atk: " + Player.attack);
+  $("#spd").val("Spd: " + Player.speed);
+  $("#def").val("Def: " + Player.defense);
+  $("#luk").val("Luk: " + Player.luck);
+  $("#hp").val("HP: " + Player.health);
+  for (var i = 0; i<3; i++) {
+    $("#weapon"+(i+1)).hide();
+  }
+  $("#weapon"+Player.weapon.type).show();
+}
+function updateEnemies () {
+  for (var i = 0; i<3; i++) {
+    $("#sk"+(i+1)).hide();
+    $("#zb"+(i+1)).hide();
+    $("#sp"+(i+1)).hide();
+    $("#hp"+(i+1)).hide();
+  }
+  for (var i = 0; i < room.enemies.length; i++) {
+    var enemy = room.enemies[i]
+    switch (enemy.type) {
+      case 1:
+        $("#sp"+(i+1)).show();
+        break;
+      case 2:
+        $("#sk"+(i+1)).show();
+        break;
+      case 3:
+        $("#zb"+(i+1)).show();
+      break;
+    }
+    $("#hp"+(i+1)).show();
+    $("#hp"+(i+1)).val("HP: " + enemy.health);
+  }
+}
+function animateEnemy () {}
+function animatePlayer () {}
